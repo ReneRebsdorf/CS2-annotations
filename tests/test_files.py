4a0365c107f24117acad739daa3ef2daff2eb2ee
Rene Rebsdorf <renerebsdorf@gmail.com>
2024-11-28 00:16:51 +0100

    Added pseudo code for new tests

--- tests/test_files.py
+++ tests/test_files.py
@@ -6,10 +6,26 @@ import keyvalues3 as kv3
 test_files = []
 for file_name in os.listdir():
     if file_name.startswith("de_") and file_name.endswith(".txt"):
-        test_files.append(file_name)
+        # Test the file can parse
+        dict = kv3.read(file_name)
 
+        @pytest.mark.parametrize("dict", dict)
+        def test_that_parser_works(dict):
+            assert dict is not None
 
-@pytest.mark.parametrize("file_name", test_files)
-def test_that_parser_works(file_name):
-    bt_config = kv3.read(file_name)
-    assert bt_config is not None
+        # Get the annotations where the key is MapAnnotationNodeX
+        annotations = []
+        for key in dict:
+            if key.startswith("MapAnnotationNode"):
+                annotations.append(dict[key])
+
+        # Test the annotations
+        @pytest.mark.parametrize("annotation", annotations)
+        def test_annotations_are_blue_or_yellow(annotation):
+            color = annotation["color"]
+            assert color == [151, 201, 250] or color == [234, 191, 86]
+
+        def test_annotations_have_no_placeholders(annotation):
+            placeHolderKey = annotation["Desc"]["Text"] 
+            assert placeHolderKey != "standing instructions"
+            assert placeHolderKey != "aim instructions"
