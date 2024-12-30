6f7b4eb552b92ce8aeaaa7be4ae2344573929979
Rene Rebsdorf <rene.rebsdorf@fellowmind.dk>
2024-12-30 13:54:49 +0100

    add args to python script

--- src/workshop.py
+++ src/workshop.py
@@ -1,9 +1,9 @@
 import os
-import subprocess  # nosec - This is a trusted command
+import subprocess  # nosec - Used to run SteamCMD
+import argparse
 
 # TODO: Ensure sub-process calls throw exceptions on error
 # TODO: Write tests
-# TODO: Look into reducing code logic and duplication in github workflow files
 
 # Map folder names to their corresponding published_file_id
 # At the time of writing, I have only found this to be working
@@ -42,51 +42,69 @@ def generate_vdf(output_path, content_folder, preview_file, title,
         output.write(vdf_content)
 
 
-# Iterate through sub-folders and upload items
-base_folder = "local"
-for folder_name in os.listdir(base_folder):
-    map_name = folder_name
-    map_path = os.path.join(base_folder, map_name)
-
-    # file name is the same as the folder name with .txt
-    file_name = map_name + ".txt"
-    content_file = os.path.join(map_path, file_name)
-    preview_file = os.path.join("assets", map_name, ".PNG")
-
-    # Check if required files exist
-    if not os.path.exists(content_file):
-        raise FileNotFoundError(f"Content file missing for {map_name}.")
-    if not os.path.exists(preview_file):
-        raise FileNotFoundError(f"Preview file missing for {map_name}.")
-
-    # Check if the folder name has a corresponding published_file_id
-    if map_name not in published_file_id_map:
-        raise KeyError(f"No published_file_id found for {map_name}.")
-
-    # Generate metadata for the VDF
-    title = f"zitrez {map_name} annotations"
-    url = "https://github.com/ReneRebsdorf/CS2-annotations"
-    description = f"Map annotations from {url}"
-    published_file_id = published_file_id_map[map_name]
-
-    # Create a temporary VDF file for this folder
-    temp_vdf = os.path.join(map_path, "metadata.vdf")
-    generate_vdf(
-        temp_vdf, map_path, preview_file, title,
-        description, published_file_id
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--steam_cmd",
+        help="Path to the SteamCMD executable",
+        required=True
     )
+    parser.add_argument(
+        "--steam_account_name",
+        help="Steam account name to login with",
+        required=True
+    )
+    args = parser.parse_args()
+    steam_cmd = args.steam_cmd
+    steam_account_name = args.steam_account_name
 
-    # Run SteamCMD to upload the item
-    subprocess.run([  # nosec - This is a trusted command
-        os.environ['STEAM_CMD'],
-        "+login",
-        os.environ['STEAM_ACCOUNT_NAME'],
-        "+workshop_build_item", temp_vdf,
-        "+quit"
-    ], check=True)
-    print(f"Successfully uploaded {map_name}.")
+    # Iterate through sub-folders and upload items
+    base_folder = "local"
+    for folder_name in os.listdir(base_folder):
+        map_name = folder_name
+        print(f"Processing {map_name} ...")
 
-    # Clean up temporary VDF file
-    os.remove(temp_vdf)
+        map_path = os.path.join(base_folder, map_name)
 
-print("All items processed.")
+        # file name is the same as the folder name with .txt
+        file_name = map_name + ".txt"
+        content_file = os.path.join(map_path, file_name)
+        preview_file = os.path.join("assets", map_name, ".PNG")
+
+        # Check if required files exist
+        if not os.path.exists(content_file):
+            raise FileNotFoundError(f"Content file missing: {content_file}")
+        if not os.path.exists(preview_file):
+            raise FileNotFoundError(f"Preview file missing: {preview_file}")
+
+        # Check if the folder name has a corresponding published_file_id
+        if map_name not in published_file_id_map:
+            raise KeyError(f"No published_file_id found for {map_name}.")
+
+        # Generate metadata for the VDF
+        title = f"zitrez {map_name} annotations"
+        url = "https://github.com/ReneRebsdorf/CS2-annotations"
+        description = f"Map annotations from {url}"
+        published_file_id = published_file_id_map[map_name]
+
+        # Create a temporary VDF file for this folder
+        temp_vdf = os.path.join(map_path, "metadata.vdf")
+        generate_vdf(
+            temp_vdf, map_path, preview_file, title,
+            description, published_file_id
+        )
+
+        # Run SteamCMD to upload the item
+        subprocess.run([  # nosec - This is a trusted command
+            steam_cmd,
+            "+login",
+            steam_account_name,
+            "+workshop_build_item", temp_vdf,
+            "+quit"
+        ], check=True)
+        print(f"Successfully uploaded {map_name}.")
+
+        # Clean up temporary VDF file
+        os.remove(temp_vdf)
+
+    print("All items processed.")
