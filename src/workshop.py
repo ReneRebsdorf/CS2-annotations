8ebf2f5c96231bbff6396273156ac41a3538128e
Rene Rebsdorf <rene.rebsdorf@fellowmind.dk>
2025-01-01 15:27:35 +0100

    Convert to using a workshop metadata template file

--- src/workshop.py
+++ src/workshop.py
@@ -2,47 +2,47 @@ import os
 import subprocess  # nosec - Used to run SteamCMD
 import argparse
 
-# TODO: Ensure sub-process calls throw exceptions on error
-# TODO: Write tests
-
-# Map folder names to their corresponding published_file_id
-# At the time of writing, I have only found this to be working
-# when the map is already uploaded to the workshop using the
-# workshop_annotation_submit command in the game console.
-published_file_id_map = {
-    "de_ancient": "",  # TODO: Replace with actual Published File IDs
-    "de_anubis": "",
-    "de_dust2": "",
-    "de_inferno": "",
-    "de_mirage": "",
-    "de_nuke": "3393679779",
-    "de_overpass": "",
-    "de_train": "",
-    "de_vertigo": "",
-}
-
 
 # Function to generate the VDF content
 def generate_vdf(output_path, content_folder, preview_file, title,
                  description, published_file_id):
-    vdf_content = f"""
-    "workshopitem"
-    {
-        "appid"            "730"
-        "contentfolder"    "{content_folder}"
-        "previewfile"      "{preview_file}"
-        "visibility"       "0"
-        "title"            "{title}"
-        "description"      "{description}"
-        "publishedfileid"  "{published_file_id}"
-    }
-    """
 
+    # Import the template file content
+    cwd = os.getcwd()
+    template_file_name = "workshop-metadata-template.vdf"
+    template_file_path = os.path.join(cwd, "src", template_file_name)
+    with open(template_file_path, "r") as file:
+        vdf_content = file.read()
+
+    # Modify placeholders
+    vdf_content = vdf_content.replace("{content_folder}", content_folder)
+    vdf_content = vdf_content.replace("{preview_file}", preview_file)
+    vdf_content = vdf_content.replace("{title}", title)
+    vdf_content = vdf_content.replace("{description}", description)
+    vdf_content = vdf_content.replace("{published_file_id}", published_file_id)
+
+    # Write the modified content to the output file
     with open(output_path, "w") as output:
         output.write(vdf_content)
 
 
 if __name__ == "__main__":
+    # Map folder names to their corresponding published_file_id
+    # At the time of writing, I have only found this to be working
+    # when the map is already uploaded to the workshop using the
+    # workshop_annotation_submit command in the game console.
+    published_file_id_map = {
+        "de_ancient": "",  # TODO: Replace with actual Published File IDs
+        "de_anubis": "",
+        "de_dust2": "",
+        "de_inferno": "",
+        "de_mirage": "",
+        "de_nuke": "3393679779",
+        "de_overpass": "",
+        "de_train": "",
+        "de_vertigo": "",
+    }
+
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--steam_cmd",
